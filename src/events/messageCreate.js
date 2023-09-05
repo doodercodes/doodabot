df940cc2feb2ac736b356d579f92bd96b8482cab
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-09-05 17:20:58 -0400

    update client events

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,9 +1,20 @@
 module.exports = async (client, message) => {
-  let prefix = client.prefix;
-  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-
   if (message.author.bot || message.channel.type === 'dm') return;
 
+  let prefix = client.prefix;
+
+  let guildDB = await client.getGuild(message.guild.id);
+  if (guildDB && guildDB.prefix) prefix = guildDB.prefix;
+
+  //Initialize GuildDB
+  if (!guildDB) {
+    await client.database.guild.set(message.guild.id, {
+      prefix: prefix,
+    });
+    guildDB = await client.getGuild(message.guild.id);
+  }
+
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
   prefix = message.content.match(prefixMention)
     ? message.content.match(prefixMention)[0]
     : prefix;
@@ -18,4 +29,23 @@ module.exports = async (client, message) => {
   const cmd =
     client.commands.get(command) ||
     client.commands.find((x) => x.aliases && x.aliases.includes(command));
+
+  if (cmd) {
+    console.log('Command was executed:'.green);
+    console.log(cmd);
+    console.log();
+    const perms = cmd.permissions;
+    if (
+      (perms &&
+        perms.channel &&
+        !message.channel.permissionsFor(client.user).has(perms.channel)) ||
+      (perms &&
+        perms.member &&
+        !message.channel.permissionsFor(message.member).has(perms.member))
+    )
+      return client.sendError(message.channel, 'Missing permissions');
+
+    // run command
+    cmd.run(client, message, args, { guildDB });
+  } else return;
 };
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,21 @@
 module.exports = async (client) => {
-  console.log(`\r\nSuccessfully logged in as: ${client.user.tag}`.green);
+  // https://discord.com/api/oauth2/authorize?client_id=1131976914458906765&permissions=8&scope=bot%20applications.commands
+  const inviteURL = `https://discord.com/oauth2/authorize?client_id=${
+    client.user.id
+  }&permissions=${
+    client.botconfig.Permissions
+  }&scope=bot%20${client.botconfig.Scopes.join('%20')}`;
+
+  client.user.inviteURL = inviteURL;
+
   client.user.setPresence({
     activities: [{ name: `${client.botconfig.Presence.name}` }],
     status: `${client.botconfig.Presence.status}`,
   });
+
+  console.log(
+    '\r\nSuccessfully logged in as: '.green +
+      `${client.user.tag}\r\n`.cyan.underline
+  );
   // client.RegisterSlashCommands();
 };
