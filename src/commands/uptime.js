befc385a195baa1559efab6e010b90bace82ac9e
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-10-08 21:03:20 -0400

    Create uptime.js

---
+++ src/commands/uptime.js
@@ -0,0 +1,19 @@
+const obj = {
+  name: 'uptime',
+  description: '',
+  usage: '',
+  permissions: {
+    channel: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS'],
+    member: [],
+  },
+  aliases: [],
+  run: async (client, message) => {
+    const days = Math.floor(client.uptime / 86400000);
+    const hrs = Math.floor(client.uptime / 3600000) % 24;
+    const minutes = Math.floor(client.uptime / 60000) % 60;
+    const seconds = Math.floor(client.uptime / 1000) % 60;
+    const uptime = `__Uptime:__\n${days}d ${hrs}h ${minutes}m ${seconds}s`;
+    message.channel.send(uptime);
+  },
+};
+module.exports = obj;
