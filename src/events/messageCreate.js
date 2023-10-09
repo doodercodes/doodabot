25b0f38882118fcf1ff73efb9a91c2a11fda88df
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-10-08 21:04:35 -0400

    Update messageCreate.js
    Comments and removing console logs

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,9 +1,7 @@
 module.exports = async (client, message) => {
   if (message.author.bot || message.channel.type === 'dm') return;
 
-  let prefix = client.prefix;
-
-  let guildDB = await client.getGuild(message.guild.id);
+  /*   let guildDB = await client.getGuild(message.guild.id);
   if (guildDB && guildDB.prefix) prefix = guildDB.prefix;
 
   //Initialize GuildDB
@@ -12,28 +10,34 @@ module.exports = async (client, message) => {
       prefix: prefix,
     });
     guildDB = await client.getGuild(message.guild.id);
-  }
+  } */
 
+  // Define the prefix for commands (DefaultPrefix is assumed to be defined elsewhere)
+  let prefix = client.botconfig.DefaultPrefix;
+
+  // Create a regular expression to match the bot mention as a prefix
   const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+
+  // Update the prefix if the message starts with a mention of the bot
   prefix = message.content.match(prefixMention)
     ? message.content.match(prefixMention)[0]
     : prefix;
 
+  // If the message doesn't start with the prefix, ignore it
   if (message.content.indexOf(prefix) !== 0) return;
 
+  // Split the message into an array of arguments and the command
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  //Making the command lowerCase because our file name will be in lowerCase
   const command = args.shift().toLowerCase();
 
-  //Searching a command
+  // Find the command in the bot's registered commands or aliases
   const cmd =
     client.commands.get(command) ||
     client.commands.find((x) => x.aliases && x.aliases.includes(command));
 
+  // If the command is found
   if (cmd) {
-    console.log('Command was executed:'.green);
-    console.log(cmd);
-    console.log();
+    // Check if the user has the required permissions to run the command
     const perms = cmd.permissions;
     if (
       (perms &&
@@ -45,7 +49,9 @@ module.exports = async (client, message) => {
     )
       return client.sendError(message.channel, 'Missing permissions');
 
-    // run command
-    cmd.run(client, message, args, { guildDB });
-  } else return;
+    // Run the command
+    cmd.run(client, message, args);
+  }
+  // If the command is not found, do nothing
+  else return;
 };
