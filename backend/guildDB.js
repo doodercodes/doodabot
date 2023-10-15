c8564d6f4b89f4ad247c6647e60c93391cad0062
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-10-15 03:10:44 -0400

    fix host variable

--- backend/guildDB.js
+++ backend/guildDB.js
@@ -6,6 +6,7 @@ class GuildDB {
   db = 's35616_doodabot';
   pool = mysql.createPool({
     //  connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PW,
