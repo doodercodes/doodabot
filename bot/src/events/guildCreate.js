e35909b54c963ea87b1d82a15724057dae660e03
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-10-20 22:25:55 -0400

    update guildCreate/Delete
    - parameterized queries to prevent SQL injection
    - remove unnecessary code
    - maintained a consistent code structure and better organization

--- bot/src/events/guildCreate.js
+++ bot/src/events/guildCreate.js
@@ -6,44 +6,34 @@ async function processGuildAndMembers(guild, members) {
   for (const member of members.values()) await memberQueries(guild, member);
 }
 
-async function guildQueries(guild, members) {
-  await db.query(`use ${db.db}`);
-
-  await db
-    .query(
-      `INSERT INTO Guilds (guild_id, name)
-      VALUES ('${guild.id}', '${guild.name}');`
-    )
-    .catch(async (err) => {
-      await db.query(
-        `
-          UPDATE Guilds
-          SET is_member = '1', joined_date = CURRENT_TIMESTAMP
-          WHERE guild_id = '${guild.id}';
-      `
-      );
-    });
+async function guildQueries(guild) {
+  try {
+    await db.query('INSERT INTO Guilds (guild_id, name) VALUES (?, ?)', [
+      guild.id,
+      guild.name,
+    ]);
+  } catch (err) {
+    await db.query(
+      'UPDATE Guilds SET is_member = 1, joined_date = CURRENT_TIMESTAMP WHERE guild_id = ?',
+      [guild.id]
+    );
+  }
 }
 
 async function memberQueries(guild, member) {
   try {
-    await db.query(`use ${db.db}`);
     await db.query(
-      `INSERT IGNORE INTO Members (guild_id, user_id, username)
-      VALUES ('${guild.id}', '${member.user.id}', '${member.user.username}');`
+      'INSERT IGNORE INTO Members (guild_id, user_id, username) VALUES (?, ?, ?)',
+      [guild.id, member.user.id, member.user.username]
     );
   } catch (err) {
     console.error(`Error inserting into Members: ${err}`);
   }
 }
 
-module.exports = async (client, guild) => {
-  const members = await guild.members.fetch();
-
-  // Filter out bot members
-  const humans = members.filter((member) => !member.user.bot);
-
-  await db.connection(async (_) => {
-    await processGuildAndMembers(guild, humans);
-  });
+module.exports = async (_, guild) => {
+  await processGuildAndMembers(
+    guild,
+    guild.members.fetch().filter((member) => !member.user.bot)
+  );
 };
