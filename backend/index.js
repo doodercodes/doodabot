3d6e96130bcc5f63060cb575981bdf7c5ab3d301
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-12-02 18:48:04 -0500

    database

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-/* 
-BACKEND IS A WIP AND WILL BE RECONSTRUCTED AS WELL
-*/
-
-const GuildDB = require('../backend/guildDB');
-const db = new GuildDB();
-
-db.init();
--- dooda/core/mechanics/database.js
+++ dooda/core/mechanics/database.js
@@ -1,20 +1,10 @@
 const mysql = require('mysql');
 const util = require('util');
 
-/* class Setup {
-  constructor() {
-    this.pool = mysql.createPool({});
-    this.query;
-  }
-} */
-
 class Database {
-  constructor(bot, db_cfg) {
+  constructor(bot) {
     this.bot = bot;
-    this.db_cfg = db_cfg;
-    this.db_name = this.db_cfg.database;
-  }
-  createPool() {
-    //
   }
 }
+
+module.exports = Database;
