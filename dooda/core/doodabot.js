4bca14c11ab85d35d03e688d11b337a841d41361
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2023-12-19 19:53:56 -0500

    a few fixes

--- dooda/core/doodabot.js
+++ dooda/core/doodabot.js
@@ -3,23 +3,43 @@ const ModuleManager = require('./mechanics/modman');
 const Config = require('./mechanics/config');
 const Logger = require('./mechanics/logger');
 
-const config = new Config();
+const cfg = new Config();
 
 class Doodabot extends Client {
   constructor() {
     super({
-      intents: config.dsc.intents,
+      intents: cfg.dsc.intents,
     });
-    this.commands = new Collection();
+    this.cfg = cfg;
+    this.modules;
+    this.db;
     this.log = new Logger();
-    this.config = config;
-    this.prefs = this.config.prefs;
-    new ModuleManager(this);
+    this.cmds = new Collection();
+
+    this.setupHook();
+  }
+
+  async setupHook() {
+    this.modules = this.initModules();
+    this.db = await this.initDatabase();
+  }
+
+  async initDatabase() {
+    this.log.info('Connecting to Database...');
+    const db = new Database(this, this.cfg.db);
+    // await db.init();
+    return db;
+  }
+
+  initModules() {
+    this.log.info('Initializing Modules...');
+    return new ModuleManager(this);
   }
 
   async build() {
     try {
-      await this.login(this.config.dsc.token);
+      this.log.info('Logging into Client...');
+      await this.login(this.cfg.dsc.token);
     } catch (err) {
       this.log.error(err.message);
     }
